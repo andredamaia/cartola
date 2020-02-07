ee3751c464a77d4f1094507b7aaf67262822abc6
andredamaia <andre.jr91@gmail.com>
2020-02-07 00:18:30 -0200

    feito até a parte dos serviços

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Banner from './Banner';
 
 
 import bgsobre from './assets/bg-sobre.png';
+import deixeacartola from './assets/deixe-a-cartola.jpg';
+import casageraldo from './assets/casa-geraldo.jpg';
+import servicos from './assets/servicos.png';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
@@ -38,12 +42,61 @@ function App() {
         </div>
       </div>
 
-     
-     
-     
-     
-     
-     
+      <div className="deixe-a-cartola">
+        <div className="container">
+            <div className="row">
+              <div className="col-12"></div>
+            </div>
+        </div>    
+      </div>
+
+      <div className="portifolio">
+        <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h2 className="text-portifolio">Portfólio</h2>
+
+                <p className="text-black"><strong>A Cartola</strong> já vestiu várias empresas de <i>Joinville e região.</i> Redesenhamos marcas com reformulação de identidade visual, reinventamos sites e cuidamos da comunicação e materiais gráficos sob o olhar exigente de quem usa o Cartola.</p>
+
+                <div className="clients-holder">
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                  <div><img src={casageraldo} alt="" /></div>
+                </div>
+              </div>
+            </div>
+        </div>    
+      </div>
+
+      <div className="bg-white">
+        <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <p className="bg-white-text">Mais clientes de <strong>Cartola</strong> </p>
+              </div>
+            </div>
+        </div>    
+      </div>
+
+      <div className="bg-yellow">
+        <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <img src={servicos} alt="" />
+              </div>
+            </div>
+        </div>    
+      </div>
+
 
     </>
   )
